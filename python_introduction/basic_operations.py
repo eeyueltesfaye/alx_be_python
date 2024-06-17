d58eca25de6bf94f97a83f20831f7ba07a51ae75
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-06-17 19:52:27 +0300

    added arithmetic exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+dif = number2 - number1
+product = number1 * number2
+
+print("Addition of " + str(number1) + " and "+ str(number2 )+ " is " + str(sum ))
+
+print (f"Subtraction of {number2} and {number1} is {dif}")
+
+print(f"Multiplication of {number2} and {number1} is {product}")

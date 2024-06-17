c8e15d1b67369fbb4e6cffa953536e11f016d17c
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-06-17 20:40:36 +0300

    added new

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,9 @@
-p = 1000 # Principal amount ($1000)
-r = 0.05    # Annual interest rate (5%)  
-t = 3        # Time (3 years)
-i = p * r * t
-print(f"The simple interest is: { i}")
+p =" principal"
+r = "rate"
+t = "time"
+
+principal = 1000 # Principal amount ($1000)
+rate = 0.05    # Annual interest rate (5%)  
+time = 3        # Time (3 years)
+interest = principal * rate * time
+print(f"The simple interest is: { interest}")

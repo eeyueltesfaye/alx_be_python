ccc32f5f84d18ee31666adc2b121f411bd19aa85
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-06-17 20:26:49 +0300

    added new change

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
-principal = 1000
-rate = 0.05
-time = 3
+principal = 1000 # Principal amount ($1000)
+rate = 0.05    # Annual interest rate (5%)  
+time = 3        # Time (3 years)
 interest = principal * rate * time
 print(f"The simple interest is: { interest}")

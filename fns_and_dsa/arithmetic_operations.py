efdccb62ed0d0080dfae8a3cf0b29965c42b4955
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-07-07 16:54:47 +0300

    updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
         if num2 == 0:
             return "Error: Division by zero"
         else:
-            return num1 / num2
+            return num1 / num2 
     else: 
         return "Error: Invalid operation"
         

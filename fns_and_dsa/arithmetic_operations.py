744f5b0a531741539cb6e8032424f7e6da8dbd59
Eyuel Tesfaye <163458107+eeyueltesfaye@users.noreply.github.com>
2024-07-07 16:32:12 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1,num2,operation):
+def perform_operation("num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":

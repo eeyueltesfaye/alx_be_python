e12ee9e311f49af6e6fde6ad1faea6a8f1290120
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-07-07 16:23:13 +0300

    added new file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1 , num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error : Division by zero"
+    else: 
+        return "Error: Invalid operation"
+        
+    

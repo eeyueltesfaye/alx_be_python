3397d20a772ec89eaca3bf4cf89a6b23cb3cd194
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-07-21 20:24:22 +0300

    added test code for class_static_methods_demo.py

---
+++ oop/test_class_static_methods_demo.py
@@ -0,0 +1,13 @@
+from class_static_methods_demo import Calculator
+
+def main():
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
+
+if __name__ == "__main__":
+    main()
